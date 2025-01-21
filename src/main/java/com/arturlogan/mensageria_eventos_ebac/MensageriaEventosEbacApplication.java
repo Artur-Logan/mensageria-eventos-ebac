fa0ce6b6ca840b558ae442ef1be72f10c4ed1903
Artur Logan <arturlogan1ferreira@gmail.com>
2025-01-21 15:50:18 -0300

    Primeiro Commit

---
+++ src/main/java/com/arturlogan/mensageria_eventos_ebac/MensageriaEventosEbacApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.mensageria_eventos_ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageriaEventosEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageriaEventosEbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturlogan/mensageria_eventos_ebac/MensageriaEventosEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.mensageria_eventos_ebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensageriaEventosEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
